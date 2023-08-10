59060004c7b325a83302059bceaa73b76e6ee818
Dan <95190809+danielwcato@users.noreply.github.com>
2023-08-10 12:49:39 -0400

    JS Calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const res = document.getElementById("result");
+
+function calculate(value) {
+  const calculatedValue = eval(value || null);
+  if (isNaN(calculatedValue)) {
+    res.value = "Can't divide 0 with 0";
+    setTimeout(() => {
+      res.value = "";
+    }, 1300);
+  } else {
+    res.value = calculatedValue;
+  }
+}
+
+function enterValue(enteredValue) {
+  if (!res.value) {
+    res.value = "";
+  }
+  res.value += enteredValue;
+}
+
+function keyboardInputHandler(e) {
+  if (e.key === "+") {
+    res.value += "+";
+  } else if (e.key === "-") {
+    res.value += "-";
+  } else if (e.key === "*") {
+    res.value += "*";
+  } else if (e.key === "/") {
+    res.value += "/";
+  }
+}
